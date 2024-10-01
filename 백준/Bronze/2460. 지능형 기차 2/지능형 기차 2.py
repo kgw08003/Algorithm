8bc43b323f0bd61383b9b0430d13eaae70aee70e
jiung <kgw08003@naver.com>
2024-10-01 17:36:18 +0900

    Update 지능형 기차 2.py

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -1,9 +1,9 @@
 a = 0
-max_a = 0
-psg = 0
+max_a = 0 
+psg = 0 # 열차 내 현재 승객 수를 저장할 변수 초기화
 
 for i in range(10):
-    out_a, in_a = map(int, input().split())
-    psg += (in_a - out_a)
-    max_a = max(psg, max_a)
+    out_a, in_a = map(int, input().split()) # 하차 승객, 승차 승객 수 입력받기
+    psg += (in_a - out_a) # 현재 승객 수 업데이트
+    max_a = max(psg, max_a) # 퇴대 승객 수 업데이트
 print(max_a)
